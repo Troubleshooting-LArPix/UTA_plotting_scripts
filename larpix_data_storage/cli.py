371e06e4bb62ea6d375b009759bfe96d2141e19a
JamesdeLeon <jdeleonphys@gmail.com>
2021-06-11 17:46:13 -0500

    initial menu and init developed

--- larpix_data_storage/cli.py
+++ larpix_data_storage/cli.py
@@ -1,5 +1,7 @@
 # COMMAND LINE INTERFACE CLASS
 
+# do enough to create a data.json file from scatch with the current test data
+
 import json
 import glob
 from file import File
@@ -25,4 +27,44 @@ class CLI:
 		- a menu showing a user which actions to perform
 	'''
 	def __init__(self):
+		print("Initializing interface...")
+		self.init_file = File('', [False, False, True])
+		self.init_data = self.init_file.open_file()
+		print("Interface initialized!")
+
+
+	def display_menu(self):
+		print("Welcome to the LArPix Database, choose an option...")
+		print("(1) Add a new tile to the database") # input serial number, initialize chips, uarts, and configs
+		print("(q) Save and exit program")
+		print('\n')
+
+
+	def add_tile(self):
 		pass
+
+
+	def save(self):
+		pass
+
+
+	def run(self):
+		while True:
+			self.display_menu()
+			menu_input = str(input("Enter an option: ")).lower()
+
+			if menu_input == 'q':
+				self.save()
+				break
+			elif menu_input == '1':
+				self.add_tile()
+				continue
+			else:
+				print("Not an acceptable input! Try again!")
+				continue
+
+
+if __name__ == "__main__":
+	interface = CLI()
+	interface.run()
+
